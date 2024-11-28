2c1794852194f399d893ba925a77784ee79d02b7
cyj <cyj@naver.com>
2024-11-28 16:45:30 +0900

    틱택톡 완성 전 단계

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,75 @@
-import { useState } from 'react';
+import { act, useState } from 'react';
 
 import Player from './conponents/Player';
 import GameBoard from './conponents/GameBoard';
 import Log from './conponents/Log';
+import GameOver from './conponents/GameOver';
+import { WINNING_COMBINATIONS } from './winning-combinations';
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
 
 function App() {
-  const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]); //턴 관련
+  //const [hasWinner, setHasWinner] = useState(false);
 
-  function handleSelectSquare() {
-    setActivePlayer((curGameBoard) => (curGameBoard === 'X' ? 'O' : 'X'));
+  //const [activePlayer, setActivePlayer] = useState('X'); // 플레이어가 X인지 O인지 구별
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    //setActivePlayer((curGameBoard) => (curGameBoard === 'X' ? 'O' : 'X'));
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+      return updatedTurns;
+    });
   }
 
   return (
@@ -27,12 +87,10 @@ function App() {
             isActive={activePlayer === 'O'}
           />
         </ol>
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          activePlayerSymbol={activePlayer}
-        />
+        {(winner || hasDraw) && <GameOver winner={winner} />}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   );
 }
